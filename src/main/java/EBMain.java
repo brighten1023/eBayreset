b36b6c2e15ed557d7a4c7c3bc93a3fafafc569a4
Bright <bright.zhou@emallcoder.com>
2021-04-23 14:15:02 -0400

    EnhancedInfo.tsv

--- src/main/java/EBMain.java
+++ src/main/java/EBMain.java
@@ -5,14 +5,13 @@ import java.util.HashSet;
 public class EBMain {
     public static void main(String[] args) throws IOException {
         HashSet<String> ch = new HashSet<String>();
-        CateReset.cateReset("123>456>789","abc>def>hij",ch);
+        /*CateReset.cateReset("123>456>789","abc>def>hij",ch);
         CateReset.cateReset("123>456>789>456","abc>def>hij>???",ch);
-        CateReset.cateReset("123>456>54646757","abc>def>yyy",ch);
-        /*String readPath = "/home/bright/Documents/eBayScrapy/Info.tsv";
+        CateReset.cateReset("123>456>54646757","abc>def>yyy",ch);*/
+        String readPath = "/home/bright/Documents/eBayScrapy/Info.tsv";
         String writePath = "/home/bright/Documents/eBayScrapy/EnhancedInfo.tsv";
-        String appId = "JialingH-Emacoder-PRD-dcbc14c02-0e31668f";
-        //System.out.println(GetPathCode.getPathCode(appId,"181148"));
-        try{
+        String appId = "BrightZh-EmallCod-PRD-9cbad4bc8-0e152d41";
+       try{
             File reader = new File(readPath);
             File writer = new File(writePath);
             BufferedWriter writePage = new BufferedWriter(new FileWriter(writer,true));
@@ -30,6 +29,6 @@ public class EBMain {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 }
